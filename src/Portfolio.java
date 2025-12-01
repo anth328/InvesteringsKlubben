5f2e39cb2843bdfde23fe7952b1b48c94d897237
Mario <64076772+supermario6469@users.noreply.github.com>
2025-12-01 11:36:12 +0100

    udfyldt klasser

--- src/Portfolio.java
+++ src/Portfolio.java
@@ -1,2 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Portfolio {
+
+    private float balance;
+    private int maengde;
+    private String ticker;
+
+    private ArrayList<Aktie> egetAktier =  new ArrayList<>();
+
+
+
+    public Portfolio(float balance, int maengde, String ticker){
+
+        this.balance = balance;
+        this.maengde = maengde;
+        this.ticker = ticker;
+
+    }
+
+
 }
--- src/Valuta.java
+++ src/Valuta.java
@@ -1,2 +1,19 @@
+import java.time.LocalDate;
+
 public class Valuta {
+
+    private String currency;
+    private String qouteCurrency;
+    private float rate;
+    private LocalDate lastUpdate;
+
+
+    public Valuta(String currency, String qouteCurrency, float rate, LocalDate lastUpdate){
+
+        this.currency = currency;
+        this.qouteCurrency = qouteCurrency;
+        this.rate = rate;
+        this.lastUpdate = lastUpdate;
+
+    }
 }
