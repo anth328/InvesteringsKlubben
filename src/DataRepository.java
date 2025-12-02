44a15ce5eb285ffe6335f6e459b035346ce277c3
megamindEK <granslev.dk@gmail.com>
2025-12-02 15:20:16 +0100

    Dp3

--- src/DataRepository.java
+++ src/DataRepository.java
@@ -69,6 +69,7 @@ public class DataRepository {
         return brugerListe;
     }
 
+
     public DataRepository() {
         aktier = new ArrayList<>();
         portfolioList = new ArrayList<>();
